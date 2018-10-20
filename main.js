bc211e5d24f3f06df53325577c83fda2d585bd30
fitsumbeyene <beyeneaddis@yahoo.com>
2018-10-20 14:57:04 -0700

    added 4 pop up windows

---
+++ main.js
@@ -0,0 +1,13 @@
+let username = prompt("what is your name");
+
+let nameconform = conform ("just cheking ,is"+username+fitsum);
+if(nameconform){ 
+    alert("welcome to my page," + username +"!");
+}
+else{ username=promp("okay,what is yourname?");
+
+
+console.log("the user name is"+username+"!");
+
+}
+
